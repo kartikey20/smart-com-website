1277564a5e0b97e039feee791d9f05abcee59456
Kartikey <ranjankartikey1999@gmail.com>
2020-08-06 22:22:13 +0530

    Created Global Variables

---
+++ src/Global-variables.js
@@ -0,0 +1,8 @@
+const registrationDate = "";
+const email = "";
+const cameraReadyDate = "";
+const conferenceDate = "";
+export const RegistrationDate = () => registrationDate;
+export const Email = () => email;
+export const CameraReadyDate = () => cameraReadyDate;
+export const ConferenceDate = () => conferenceDate;
