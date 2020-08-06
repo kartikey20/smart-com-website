7422ab3dd8fa26cf4a6f23c267891b9b68900992
Kartikey <ranjankartikey1999@gmail.com>
2020-08-06 22:28:23 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Homepage from "./pages/Homepage";
+import Committee from "./pages/Committee";
+import PaperSubmissionGuidelines from "./pages/Paper-submission-guidelines";
+import Registration from "./pages/Registration";
+import ContactForm from "./pages/Contact-us";
+import CallForPapers from "./pages/Call-for-papers";
+import CallForSpecialSession from "./pages/Call-for-special-session";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/Committee" component={Committee} />
+          <Route
+            path="/Paper-Submission-Guidelines"
+            component={PaperSubmissionGuidelines}
+          />
+          <Route path={"/Registration"} component={Registration} />
+          <Route path={"/Contact-us"} component={ContactForm} />
+          <Route path={"/Call-For-Papers"} component={CallForPapers} />
+          <Route
+            path={"/Call-For-Special-Session"}
+            component={CallForSpecialSession}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
