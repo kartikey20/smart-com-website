e65ffbee02342219193d32279de0a1fb843154a9
Kartikey <ranjankartikey1999@gmail.com>
2020-08-06 23:20:13 +0530

    Added Link to NavLinks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,60 +1,73 @@
 import React from "react";
-import {
-  Button,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-  NavDropdown
-} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Box from "@material-ui/core/Box";
+import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <>
-      {" "}
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src="/src/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Smart Com
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link to={"/"}>Home</Nav.Link>
-            <Nav.Link to={"/Committee"}>Committee</Nav.Link>
-            <NavDropdown title="Papers" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Call for papers
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Call for special session
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Accepted papers
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
-                Registered papers
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.5">
-                Smart Com schedule
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link to={"/Paper-submission-guidelines"}>Paper submission guidelines</Nav.Link>
-            <Nav.Link to={"/Registration"}>Registration</Nav.Link>
-            <Nav.Link to={"/Contact-us"}>Contact us</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+      <Box>
+        <Navbar bg={"light"} expand="lg" variant="light">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/src/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Smart Com
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Committee">Committee</Link>
+              </Nav.Link>
+              <NavDropdown title="Papers" id="basic-nav-dropdown">
+                <NavDropdown.Item>
+                  <Link to={"/Call-For-Papers"}>Call for papers</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to={"/Call-For-Special-Session"}>
+                    Call for special session
+                  </Link>
+                </NavDropdown.Item>
+
+                <NavDropdown.Item>
+                  <Link to={"/Accepted-Papers"}>Accepted papers</Link>
+                </NavDropdown.Item>
+
+                <NavDropdown.Item>
+                  {" "}
+                  <Link to={"/Registered-Papers"}>Registered papers</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item>
+                  <Link to={"/Smart-Com-Schedule"}>Smart Com schedule</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link>
+                <Link to={"/Paper-Submission-Guidelines"}>
+                  Paper submission guidelines
+                </Link>
+              </Nav.Link>
+
+              <Nav.Link>
+                <Link to={"/Registration"}>Registration</Link>
+              </Nav.Link>
+
+              <Nav.Link>
+                <Link to={"/Contact-us"}>Contact us</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Box>
     </>
   );
 };
