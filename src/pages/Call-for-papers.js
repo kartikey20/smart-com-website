80fe21c0559bca37916e7bf760dd55536482ed64
Kartikey <ranjankartikey1999@gmail.com>
2020-08-09 20:51:36 +0530

    Added header title

--- src/pages/Call-for-papers.js
+++ src/pages/Call-for-papers.js
@@ -2,9 +2,13 @@ import React from "react";
 import Paragraph from "../components/Call-For-Paper/Paragraph";
 import CallForPapersAccordion from "../components/Call-For-Paper/Accordion";
 import Box from "@material-ui/core/Box";
+import {Typography} from "@material-ui/core";
 const CallForPapers = () => {
   return (
     <>
+        <Box m={3}>
+            <Typography variant="h2">Call For Papers</Typography>
+        </Box>
       <Box m={3}>
         <Paragraph />
         <CallForPapersAccordion />
