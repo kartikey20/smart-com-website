a95ec7b2f13a8afc5c0777748fd8c6f896fac88a
Kartikey <ranjankartikey1999@gmail.com>
2020-08-09 20:47:24 +0530

    specail session content json data

---
+++ src/components/SpecialSession/Special-Session-Content.js
@@ -0,0 +1,85 @@
+const data = [
+  {
+    title: "blockchain-technologies-for-internet-of-things-iot",
+    sessionTitle: "Blockchain Technologies for Internet of Things (IoT)",
+    speaker1: "Dr. Mohammad Tabrez Quasim",
+    speaker2: "Dr Fahad Algarni University of Bisha, Saudi Arabia,",
+    description:
+      "Theme of the Special Session Internet of things (IoT) is reshaping the cyber world by connecting everything to the internet. From industry to smart industry, from hospital to smart hospital, from cities to smart cities are few applications of IoT. The IoT ecosystem is complex and heterogenous that bring a number of challenges such as decentralization, poor interoperability, privacy and security vulnerabilities susceptible to attacks. The blockchain has emerged as the solution for IoT security due to its numerous advantages such as distributed data storage and immutability. It offers the potential for improving the overall security of the IoT ecosystem. The International Data Corporation predicts that about 20% of all IoT deployments will have some sort of Blockchain services enabled in near future. There is a strong reason to believe that blockchain will lead to the establishment of a new generation of IoT ecosystem. This special track will provide a forum for researchers and industrialists to share recent research results on the convergence of Blockchain and IoT, ranging from overviews, proof-of-concepts case studies, to applications",
+    topics: [
+      "Blockchain in IoT device authentication, authorization and access control",
+      "Blockchain in IoT data and system security",
+      "Blockchain in IoT supply chain management",
+      "Blockchain in M2M communications",
+      "Blockchain platforms for IoT development",
+      "Blockchain implementation in IoT embedded systems",
+      "Novel Blockchain microprocessors and microcontrollers",
+      "Blockchain in smart home applications",
+      "Blockchain in smart building applications",
+      "Blockchain in smart city applications",
+      "Blockchain in smart healthcare applications",
+      "Blockchain in smart transport applications",
+      "Blockchain in smart manufacturing system applications",
+      "Blockchain in smart agriculture system applications",
+      "Blockchain in smart oil industry applications",
+      "Testing, simulation and performance analysis is blockchain",
+      "Blockchain applications in IoT: case studies",
+      "Blockchain and IoT tools for teaching and learning"
+    ],
+    procedure: "ddfsdfdsff"
+  },
+  {
+    title: "circuit-system-design-and-optimization-technologies",
+    sessionTitle: "Circuit, System Design and Optimization Technologies",
+    speaker1: "Dr. Manoj Sharma, Associate Professor, BVCOE, New Delhi, India",
+    speaker2: "",
+    topics: [
+      "VLSI circuits design, low power design techniques and architecture, Reconfigurable and programmable, models, characterization, FPGA, arrays of transistors",
+      "Hardware for Communication system, Digital signal processing (DSP), Memory systems, Memory architecture, HW-SW codesign, VLSI algorithms",
+      "Emerging Technologies, MEMS, FinFET, sensors, design methodologies, nanotechnology, Biomedical circuits",
+      "Performance optimization methodologies , on-chip communication architectures, networks-on-chip System, Design validation and verification, Testing methodologies",
+      "Artificial Intelligence, machine learning, Fuzzy and other techniques and their application in VLSI, hardware design",
+      "GPU design based design, Applications in Healthcare, Smart Grid, Intelligent and Secure Transportation Systems, Safety Assurance of Embedded Circuits and Systems, Secure Embedded Circuits and Systems",
+      "IOT, Robotics , data science, effective analysis, predictive analysis, Machine learning, AI powered IoT, Robotics",
+      "New Generations of Intelligent Devices optimized for machine learning, deep learning"
+    ],
+    description:
+      "The main objective of this session is to provide the platform to the researchers, industry experts, academicians and innovative individuals to get together and discuss the all Emerging trends in IC design technologies. Technology plays major role in augmenting the human life driven with the advances in hardware capabilities. The platform hence also tries to collate efforts in VLSI and Embedded architectures, circuit, optimization and applications including IOT, Robotics and data science for effective analysis."
+  },
+  {
+    title: "artificial-intelligence-and-nature-inspired-algorithms",
+    sessionTitle: "Artificial Intelligence and Nature Inspired Algorithms",
+    speaker1: "Dr. Rijwan Khan, ABES Institute of Technology, Ghaziabad, India",
+    speaker2:
+      "Dr. Pawan Kumar Sharma , Inmantec institutions, Ghaziabad, India",
+    topics: "",
+    description: ""
+  },
+  {
+    title: "recent-trends-on-vlsi-design-and-embedded-systems",
+    sessionTitle: "Recent Trends on VLSI Design and Embedded systems",
+    speaker1: "Dr. Sanjay Kumar Singh, Professor, ABESEC Ghaziabad",
+    speaker2: "",
+    topics: "",
+    description: ""
+  },
+  {
+    title: "mathematical-modelling-for-signal-and-image-processing",
+    sessionTitle: "Mathematical Modelling for Signal and Image Processing",
+    speaker1:
+      "Dr. Nitendra Kumar, SEMS, India and Sharda University, Greater Noida, India",
+    speaker2:
+      "Dr. Santosh Kumar, Assistant Professor, Sharda University, Greater Noida, India",
+    topics: "",
+    description: ""
+  },
+  {
+    title: "recent-trends-on-vlsi-design-and-embedded-systems",
+    sessionTitle: "Recent Trends on VLSI Design and Embedded systems",
+    speaker1: "Dr. Sanjay Kumar Singh, Professor, ABESEC Ghaziabad",
+    speaker2: "",
+    topics: "",
+    description: ""
+  }
+];
+export default data;
