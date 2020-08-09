110c88327b5be8a7f2b8da3720e4f650f7d72622
Kartikey <ranjankartikey1999@gmail.com>
2020-08-09 20:53:05 +0530

    Created MemberData for Committee

---
+++ src/components/Committee/MemberData.js
@@ -0,0 +1,162 @@
+const data = {
+  "General Co-Chairs": [
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" },
+    { name: "d", designation: "", location: "" }
+  ],
+  "Conference Chair": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Technical Program Chair": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Organizing Secretaries": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Track Chair": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Publication Chair": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Advisory Committee": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Publicity Chair": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Web Committee": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Press and Media Committee": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Technical Program Committee": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Finance Committee": [
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" },
+    { name: "", designation: "", location: "" }
+  ],
+  "Hospitality Committee": [
+    {
+      name: "Mr. Rishi Raj Singh",
+      designation: "Assistant Professor",
+      location: "GBPIET, Pauri Garhwal"
+    },
+    {
+      name: "Mr. Jogendra Kumar",
+      designation: "Assistant Professor",
+      location: "GBPIET Pauri Garhwal"
+    },
+    {
+      name: "Mr. S.S Rawat",
+      designation: "Assistant Professor",
+      location: "GBPIET Pauri Garhwal"
+    },
+    {
+      name: "Mrs. Lotika Oinam",
+      designation: "Assistant Professor",
+      location: "GBPIET, Pauri Garhwal"
+    }
+    /*{ name: "", designation: "", location: "" },
+        { name: "", designation: "", location: "" },
+        { name: "", designation: "", location: "" },
+        { name: "", designation: "", location: "" },
+        { name: "", designation: "", location: "" }*/
+  ]
+};
+export default data;
